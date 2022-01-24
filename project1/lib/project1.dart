e55d071e9649bd263341a6612a9edcd46f84c4d7
sitaram <kalluriramkumar@gmail.com>
2022-01-24 08:34:49 +0530

    remove comments

--- project1/lib/project1.dart
+++ project1/lib/project1.dart
@@ -1,9 +1,6 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library project1;
 
 export 'src/project1_base.dart';
 export 'src/number_operations_spec.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+
--- project2/lib/project2.dart
+++ project2/lib/project2.dart
@@ -5,4 +5,4 @@ library project2;
 
 export 'src/project2_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+
